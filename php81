2531ba0ffc5dbc837569388c8ff4545d08ca6066
Peter Helsen <peter.helsen@kuleuven.be>
2026-01-11 15:05:16 +0100

    init

---
+++ php81
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+"/c/Workdir/MyApps/web/php/php-8.1.34-Win32-vs16-x64\php.exe" "$@"
---
+++ start_mysql.sh
@@ -0,0 +1,1 @@
+/c/Workdir/Myapps/web/mysql/bin/mysqld.exe   --basedir="/c/Workdir/MyApps/web/mysql"   --datadir="/c/Workdir/data/mysql84" --port=3306 --console
